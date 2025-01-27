78eefd0f548ae17995c69698b71e999f464b9d95
sanyaa23 <sanya.20223234@mnnit.ac.in>
2025-01-28 03:59:52 +0530

    adding changes

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import { ApiResponse } from './utils/ApiResponse.js';
-// import fileUpload from 'express-fileupload';
-//import { uploadOnCloudinary } from './utils/cloudinary.js';
-// import userRoutes from './routes/user';
-// import complaintRoutes from './routes/complaint';
 import profileRouter from './routes/profile.routes.js';
 import userRouter from './routes/user.routes.js';
 import menuRouter from './routes/menu.routes.js'
-// import menuRoutes from './routes/menu';
-// import committeeRoutes from './routes/commitee';
-// import dailyExpense from './routes/expense';
-// import ratingRoutes from './routes/rating';
+
 
 dotenv.config({
     path: './.env'
@@ -32,25 +25,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//     fileUpload({
-//         useTempFiles: true,
-//         tempFileDir: '/tmp/',
-//     })
-// );
 
-// Connect to DB and Cloudinary
 connectDB();
-//uploadOnCloudinary();
 
 // Use Routes
 app.use('/api/v1/auth', userRouter);
-// app.use('/api/v1/complaint', complaintRoutes);
 app.use('/api/v1/profile', profileRouter);
 app.use('/api/v1/menu', menuRouter);
-// app.use('/api/v1/committee', committeeRoutes);
-// app.use('/api/v1/expense', dailyExpense);
-// app.use('/api/v1/rating', ratingRoutes);
 
 // Default route
 app.get('/', (req, res) => {
@@ -59,10 +40,6 @@ app.get('/', (req, res) => {
         .json(
             new ApiResponse(200, {}, "server running")
         );
-    // return res.status(200).json({
-    //     success: true,
-    //     message: 'server is running',
-    // });
 });
 
 app.listen(port, () => {
--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -3,28 +3,11 @@ import { verifyJwt, isChiefWarden, isAccountant } from "../middleware/auth.middl
 import { updateProfile, getUserDetails, getUserByRegistrationNumber, deleteUserAccount, blockUserProfile, unblockUserProfile, markFeeStatusAsTrue, markFeeStatusAsFalse } from "../controllers/profile.controller.js";
 
 const router = Router();
-// const {
-//     auth,
-//     isChiefWarden,
-//     isAccountant,
-//     isAccountantOrIsWarden,
-// } = require("../middleware/auth.middleware");
-// const {
-//     getAllUserDetails,
-//     updateProfile,
-//     blockUser,
-//     unblockUser,
-//     deleteAccount,
-//     findUserByRegistrationNumber,
-//     updateProfilePicture,
-//     markFeeStatusTrue,
-//     markFeeStatusFalse
-// } = require("../controllers/profileController");
+
 
 
 router.get("/get-user-details", verifyJwt, getUserDetails);
 router.put("/update-profile", verifyJwt, updateProfile);
-// router.put("/updateDisplayPicture", auth, updateProfilePicture);
 router.post("/block-user", verifyJwt, isChiefWarden, blockUserProfile);
 router.delete("/unblock-user", verifyJwt, isChiefWarden, unblockUserProfile);
 router.delete("/delete-account", verifyJwt, deleteUserAccount);
